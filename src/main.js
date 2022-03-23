2d02e61401684edbce61b4c6cb4c40293e898da7
张伟峰 <wulakaka@live.com>
2022-03-23 19:59:11 +0800

    feat: 增加provide的测试组件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// 运行时的版本
+import { computed } from "vue/dist/vue.esm-bundler.js";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+// 配置自动解包
+app.config.unwrapInjectedRef = true;
+app.use(router).mount("#app");
+
+// 全局注册，注意引用时，标签的写法需要与组件名一致
+app.component("todo-list", {
+  data() {
+    return {
+      todos: ["Feed a cat", "Buy tickets"],
+    };
+  },
+  provide() {
+    return {
+      todosLength: computed(() => this.todos.length),
+    };
+  },
+  template: `
+    <div>todo
+      {{ todos.length }}
+      <!-- 模板的其余部分 -->
+      <button @click="todos.push('a')">add <todo-list-statistics/></button>
+    </div>
+  `,
+});
+
+app.component("todo-list-statistics", {
+  inject: ["todosLength"],
+  template: `
+    <span>
+      {{ todosLength }}
+      <!-- 模板的其余部分 -->
+    </span>
+  `,
+});
